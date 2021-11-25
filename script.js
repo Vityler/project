a33433986cfe8989863e20b0b84e45faa5f30ed6
Vitaliy <krvc.vitaliy@gmail.com>
2021-11-25 17:32:00 +0200

    add comments

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 "use strict";
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+//создал переменную, поместил в нее ответ на вопрос
 
-const personalMovieDB ={
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat : false
+const personalMovieDB ={//создал объект
+    count: numberOfFilms,//сюда передается первый вопрос
+    movies: {},//пустой объект
+    actors: {},//пустой объект
+    genres: [],//пустой массив
+    privat : false//булиновое значение
 };
-
+//задал пользователю 2 вопроса по 2 раза
 const a = prompt('Один из последних просмотренных фильмов?',''),
       b = prompt('на сколько оцените его?',''),
       c = prompt('Один из последних просмотренных фильмов?',''),
       d = prompt('на сколько оцените его?','');
-
+//ответы поместил отдельные переменные, записал ответы в объект movies
       personalMovieDB.movies [a] = b,
       personalMovieDB.movies [c] = d;
 
